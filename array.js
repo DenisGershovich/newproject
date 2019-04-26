166332951f073bc4ca19115e55a8053c051746ee
Denis <sus00700@gmail.com>
2019-04-26 23:27:19 +0300

    new update

---
+++ array.js
@@ -0,0 +1,92 @@
+////const array = [-1,0,3,100, 99, 2, 99] // should return 100
+////const array2 = ['a', 3, 4, 2] // should return 4
+////let array3 = [] // should return 0
+//
+//var array1 = [1, 3, 2,5,7,3,2,77,98,8,7,66];
+//var array2 = array1[0];
+//
+//array1.forEach(num => {
+//    if(array2 < num){
+//        array2 = num;
+//    }
+//});
+//
+
+//const array = [-1,0,3,100, 99, 2, 99] // should return 100
+//const array2 = ['a', 3, 4, 2] // should return 4
+//const array3 = [0] // should return 0
+//let bignum = array3[0];
+//
+////function biggestNumberInArray(arr) {
+////    if(bignum < arr){
+////        bignum = arr;
+////    }
+////}
+////
+////array.forEach(biggestNumberInArray);
+//
+//for(num of array3){
+//    if(bignum < num){
+//        bignum = num;
+//    }
+//}
+
+// Question #2:
+// Write a function checkBasket() that lets you know if the item is in the basket or not
+amazonBasket = {
+  glasses: 1,
+  books: 2,
+  floss: 100
+}
+
+for(item in amazonBasket){
+    if(item = 1){
+        console.log(item);
+    }
+    else{
+        console.log('no items');
+    }
+}
+
+
+function checkBasket(basket, lookingFor) {
+    
+    
+    
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
